47bfef9ddfb16a00e4c59480da7ee3cf4a7f4646
Mario Martinez <mauromarto07@gmail.com>
2024-03-14 18:19:18 +0000

    fix: how were added and updated the values to db

--- number_guess.sh
+++ number_guess.sh
@@ -3,19 +3,19 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 R_NUM=$((1 + $RANDOM % 1000))
 GUESSES=1
+echo "The random is $R_NUM"
 echo "Enter your username:"
 read USERNAME
 GET_USER=$($PSQL "SELECT username FROM usernames WHERE username='$USERNAME';")
 #if username is not in db
 if [[ -z $GET_USER ]]
 then
-  INSERT_USER=$($PSQL "INSERT INTO usernames(username) VALUES('$USERNAME');")
-  echo "Welcome, $USERNAME! It looks like is your first time here."
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
 #if it is
-  GAMES=$($PSQL "SELECT games FROM usernames WHERE username='$USERNAME';")
+  GAMES_PLAYED=$($PSQL "SELECT games FROM usernames WHERE username='$USERNAME';")
   BEST_GAME=$($PSQL "SELECT bestgame FROM usernames WHERE username='$USERNAME';")
-  echo -e "Welcome back, $USERNAME! You have played $GAMES games, and your best game took $BEST_GAME guesses."
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
@@ -38,3 +38,17 @@ do
   fi
 done
 echo "You guessed it in $GUESSES tries. The secret number was $R_NUM. Nice job!"
+
+if [[ -z $GET_USER ]]
+then 
+  INSERT_USER=$($PSQL "INSERT INTO usernames(username, games, bestgame) VALUES('$USERNAME', 1, $GUESSES);")
+else
+  GAMES=$((GAMES_PLAYED+1))
+  if [[ $GUESSES -lt $BEST_GAME ]]
+  then
+    BEST=$GUESSES
+  else
+    BEST=$BEST_GAME
+  fi
+  UPDATE_USER=$($PSQL "UPDATE usernames SET games=$GAMES, bestgame=$BEST WHERE username='$USERNAME';")
+fi
